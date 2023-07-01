97bc6a31711cb1df0d3fd3976c89eedc2bc91864
jenisnm <136350163+jenisnm@users.noreply.github.com>
2023-07-01 18:02:42 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig(({ command }) => {
   }
 
   if (command !== 'serve') {
-    config.base = '/jenisnm.github.io/'
+    config.base = '/jenihacker.github.io/'
   }
 
   return config
