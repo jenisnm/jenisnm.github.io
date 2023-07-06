230b0d8ad2904a3efa2a2b8f94ecf0c5c1c3fbba
Jenihacker <20cs064.jenison@sjec.ac.in>
2023-07-06 09:19:28 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react()],
-    base: '/',
-  }
-
-<<<<<<< HEAD
-=======
-  if (command !== 'serve') {
-    config.base = '/jenihacker.github.io/'
-  }
-
->>>>>>> aa0aedc19cd9a823ffb6d2080560c0fde8e33229
-  return config
+export default defineConfig({
+  plugins: [react()],
 })
